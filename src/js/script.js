44f8fc79624dfea450704c908fb69579749ba014
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-09-27 11:23:47 +0700

    create fitsr task

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,26 @@
 `use strict`;
 
+// var number = 5;
+// var string = `hello`;
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
 
+// console.log(4/0);
+// console.log(`string`*5);
 
+// let person = {
+// 		name: `John`,
+// 		age: 25,
+// 		isMarried: false
+// };
+// console.log(person[`name`]);
 
-var number = 5;
-var string = `hello`;
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+// let arr = [`plum.png`, `orange.jpg`, `apple.bmp`]
 
-console.log(4/0);
-console.log(`string`*5);
-
-let person = {
-		name: `John`,
-		age: 25,
-		isMarried: false
-};
-console.log(person[`name`]);
-
-let arr = [`plum.png`, `orange.jpg`, `apple.bmp`]
-
-console.log(arr[0]);
+// console.log(arr[0]);
 
 // let answer = confirm(`Are you here?`);
 
@@ -36,17 +33,43 @@ console.log(arr[0]);
 // console.log(`arr` + ` - object`);
 // console.log(4 + +` - object`);
 
-let 	incr = 10;
-		decr = 10;
+// let 	incr = 10;
+// 		decr = 10;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
 
-console.log(5%2);
-console.log(`2` == 2);
+// console.log(5%2);
+// console.log(`2` == 2);
 
-let 	isChecced = false;
-		isClose = true;
+// let 	isChecced = false;
+// 		isClose = true;
 
-console.log(isChecced && isClose);
+// console.log(isChecced && isClose);
+
+// alert();
+
+let 	money = prompt(`Ваш бютжет на месяц?`, ``);
+		time = prompt(`Введите дату в формате YYYY-MM-DD`, ``);
+
+let 	appData = {
+			budget: money,
+			timeData: time,
+			expenses: {},
+			optionalExpenses: {},
+			income: [],
+			savings: false
+};
+
+let  	a1 = prompt(`Введите обязательную статью расходов в этом месяце`, ``);
+		a2 = prompt(`Во сколько обойдется`, ``);
+		a3 = prompt(`Введите обязательную статью расходов в этом месяце`, ``);
+		a4 = prompt(`Во сколько обойдется?`, ``);
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
+
+console.log();
