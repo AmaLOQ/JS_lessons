0a2ddd02234b3335353c07705e92f7151fd431cf
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-09-27 10:26:21 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+`use strict`;
+
+
+
+
+var number = 5;
+var string = `hello`;
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log(`string`*5);
+
+let person = {
+		name: `John`,
+		age: 25,
+		isMarried: false
+};
+console.log(person[`name`]);
+
+let arr = [`plum.png`, `orange.jpg`, `apple.bmp`]
+
+console.log(arr[0]);
+
+// let answer = confirm(`Are you here?`);
+
+// console.log(answer);
+
+// let answer = +prompt(`Есть ли вам 18?`, `Да`);
+
+// console.log(typeof(answer));
+
+// console.log(`arr` + ` - object`);
+// console.log(4 + +` - object`);
+
+let 	incr = 10;
+		decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+
+console.log(5%2);
+console.log(`2` == 2);
+
+let 	isChecced = false;
+		isClose = true;
+
+console.log(isChecced && isClose);
